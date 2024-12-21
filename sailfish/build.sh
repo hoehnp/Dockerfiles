12fb4c755c99ab7653d15168a6bd3b6972fb0e27
Patrick Höhn <hoehnp@gmx.de>
2024-12-21 20:29:09 +0100

    update for SFOS 4.6.0.13

--- sailfish/build.sh
+++ sailfish/build.sh
@@ -1,44 +1,45 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SDK_VERSION="1807"
-TARGET_VERSION="2.2.1.18"
-SDK_NAME="Sailfish_OS-$TARGET_VERSION-Platform_SDK_Chroot-i486.tar.bz2"
+SDK_VERSION="latest"
+TARGET_VERSION="4.6.0.13"
+SDK_NAME="Jolla-$SDK_VERSION-SailfishOS_Platform_SDK_Chroot-i486.tar.bz2"
 SDK_URL="http://releases.sailfishos.org/sdk/installers/$SDK_VERSION/$SDK_NAME"
 
 
 # correct chroot file rights
-wget $SDK_URL
-bunzip2 $SDK_NAME
-tar xvf Sailfish_OS-$TARGET_VERSION-Platform_SDK_Chroot-i486.tar ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo ./etc/group ./etc/passwd
+#wget $SDK_URL
+rm -rf SDK_VERSION-SailfishOS_Platform_SDK_Chroot-i486.tar
+cp ../$SDK_NAME . 
+bunzip2 -f $SDK_NAME
+tar xvf Jolla-$SDK_VERSION-SailfishOS_Platform_SDK_Chroot-i486.tar ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile ./var/spool/mail/appsupport-root ./etc/group ./etc/passwd
 
-tar --delete --file=Sailfish_OS-$TARGET_VERSION-Platform_SDK_Chroot-i486.tar ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo ./etc/group ./etc/passwd ./home/nemo
+tar --delete --file=Jolla-$SDK_VERSION-SailfishOS_Platform_SDK_Chroot-i486.tar ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile ./var/spool/mail/appsupport-root ./etc/group ./etc/passwd ./home/appsupport-root
 
 sed -i 's/100000/1000/g' ./etc/passwd
 sed -i 's/100000/1000/g' ./etc/group
 
-chown -h 1000 ./home/nemo/ ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo
-chgrp -h 1000 ./home/nemo/ ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile
-
-tar --append --file=Sailfish_OS-$TARGET_VERSION-Platform_SDK_Chroot-i486.tar ./home/nemo/ ./var/spool/mail/nemo ./etc/group ./etc/passwd
+tar --append --file=Jolla-$SDK_VERSION-SailfishOS_Platform_SDK_Chroot-i486.tar ./etc/group ./etc/passwd
 
 rm -rf ./home ./var ./etc 
 
 # correct i486 file rights
-wget https://releases.sailfishos.org/sdk/targets-$SDK_VERSION/Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2
-bunzip2 Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2
+#wget https://releases.sailfishos.org/sdk/targets-$SDK_VERSION/Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2
+cp ../Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2 .
+bunzip2 -f Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2
 echo "before extracting second time"
-tar xvf Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo ./etc/group ./etc/passwd
-tar --delete --file=Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo ./etc/group ./etc/passwd ./home/nemo
+tar xvf Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile ./var/spool/mail/appsupport-root ./etc/group ./etc/passwd
+tar --delete --file=Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile ./var/spool/mail/appsupport-root ./etc/group ./etc/passwd ./home/appsupport-root
 echo "after extracting second time"
 sed -i 's/100000/1000/g' ./etc/passwd
 sed -i 's/100000/1000/g' ./etc/group
 
-chown -h 1000 ./home/nemo/ ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile ./var/spool/mail/nemo
-chgrp -h 1000 ./home/nemo/ ./home/nemo/.bash_logout ./home/nemo/.bashrc ./home/nemo/.bash_profile
+chown -h 1000 ./home/appsupport-root/ ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile ./var/spool/mail/appsupport-root
+chgrp -h 1000 ./home/appsupport-root/ ./home/appsupport-root/.bash_logout ./home/appsupport-root/.bashrc ./home/appsupport-root/.bash_profile
 
-tar --append --file=Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/nemo ./var/spool/mail/nemo ./etc/group ./etc/passwd
+tar --append --file=Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar ./home/appsupport-root ./var/spool/mail/appsupport-root ./etc/group ./etc/passwd
 bzip2 Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar
+md5sum Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2 Sailfish_OS-$TARGET_VERSION-Sailfish_SDK_Tooling-i486.tar.bz2.md5sum
 rm -rf ./home ./var ./etc
 
 echo "Building image"
